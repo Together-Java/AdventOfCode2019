08acf364f6579b7ee82cf9751f20027fa432f3e1
TheSexyNerd <midnite-pk@hotmail.com>
2019-12-03 02:18:46 -0500

    first part of day 3 bruteforced :)

--- src/com/togetherjava/adventofcode/day3/Day3.java
+++ src/com/togetherjava/adventofcode/day3/Day3.java
@@ -1,16 +1,18 @@
 package com.togetherjava.adventofcode.day3;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.togetherjava.adventofcode.util.ResourceLoader;
 
 public class Day3 {
 
-	private static List<Tile> wireA = new ArrayList<>();
-	private static List<Tile> wireB = new ArrayList<>();
+	private static Set<Tile> wireA = new HashSet<>();
+	private static Set<Tile> wireB = new HashSet<>();
 
 	public static void main(String[] args) {
+		long then = System.currentTimeMillis();
 		List<String> data = ResourceLoader.getInput("Day3Inputs.txt");
 		parseWire(data.get(0).split(","), wireA);
 		parseWire(data.get(1).split(","), wireB);
@@ -19,35 +21,46 @@ public class Day3 {
 		
 		int recordDistance = Integer.MAX_VALUE;
 		for (Tile t : wireA) {
-			int distance = getManhattanDistance(t.getX(), t.getY(), 0, 0);
-			if (distance < recordDistance) {
+			int distance = getManhattanDistance(0, 0, t.getX(), t.getY());
+			if (distance < recordDistance && distance != 0) {
 				recordDistance = distance;
 			}
 		}
-
-		System.out.println(recordDistance);
+		System.out.println("Part 1 answer) redacted");
+		System.out.printf("Done in %d ms", System.currentTimeMillis() - then);
 	}
 
 	public static int getManhattanDistance(int x, int y, int x2, int y2) {
 		return Math.abs(x2 - x) + Math.abs(y2 - y);
 	}
 
-	public static void parseWire(String[] data, List<Tile> wire) {
+	public static void parseWire(String[] data, Set<Tile> wire) {
 		int x = 0;
 		int y = 0;
 		for (String w : data) {
 			String direction = w.substring(0, 1);
 			int moves = Integer.parseInt(w.substring(1));
 			if (direction.equals("U")) {
+				for(int i = y; i > y - moves; i--) {
+					wire.add(new Tile(x, i));
+				}
 				y -= moves;
 			} else if (direction.equals("D")) {
+				for(int i = y; i < y + moves; i++) {
+					wire.add(new Tile(x, i));
+				}
 				y += moves;
 			} else if (direction.equals("L")) {
+				for(int i = x; i > x - moves; i--) {
+					wire.add(new Tile(i, y));
+				}
 				x -= moves;
 			} else if (direction.equals("R")) {
+				for(int i = x; i < x + moves; i++) {
+					wire.add(new Tile(i, y));
+				}
 				x += moves;
 			}
-			wire.add(new Tile(x, y));
 		}
 	}
 }
--- src/com/togetherjava/adventofcode/day3/Tile.java
+++ src/com/togetherjava/adventofcode/day3/Tile.java
@@ -18,11 +18,30 @@ public class Tile {
 	}
 	
 	@Override
-	public boolean equals(Object obj) {
-		Tile tile = (Tile) obj;
-		return tile.getX() == x && tile.getY() == y;
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
 	}
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tile other = (Tile) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() {
 		return String.format("X = %d Y = %d", x, y);
